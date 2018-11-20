cf74e8a2d197a60ddfe49508671e2cd20987ae4c
JP <30301499+jpospina88@users.noreply.github.com>
2018-11-20 14:31:42 -0800

    comment file

--- c2016_lists_variables_settings_plots.R
+++ c2016_lists_variables_settings_plots.R
@@ -7,7 +7,7 @@ library(kableExtra)
 
 # For RAs
 # getwd()
-d <- read_rds("../../../Box Sync/Research Assistants/data/2018_4_2_c2016_syfus_inst_healthrec_ctra_no_t7.rds")
+# d <- read_rds("../../../Box Sync/Research Assistants/data/2018_4_2_c2016_syfus_inst_healthrec_ctra_no_t7.rds")
 
 source("R/custom_functions.R")
 
