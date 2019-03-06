c1002d82bf0e0d271b7830877435022cd6684e3f
JP <30301499+jpospina88@users.noreply.github.com>
2019-03-05 18:21:40 -0800

    adds packages

---
+++ packages.R
@@ -0,0 +1,11 @@
+library(magrittr)
+library(RColorBrewer)
+library(reshape2)
+library(tidyverse)
+library(emmeans) # to get adjusted means and to plot the summary statistics of models
+library(kableExtra)
+library(psych)
+library(apaTables)
+library(ez)
+library(grid) # For iterative plots
+library(gridExtra) # For iterative plots
